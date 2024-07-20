7315f8dee7448e0d2ae2151b892ee7e5ee6a7bdb
Andrey Nazarov <mondegor@gmail.com>
2024-07-20 18:33:16 +0300

    See changelog 2024-07-20

--- mrserver/mrprometheus/observe_request.go
+++ mrserver/mrprometheus/observe_request.go
@@ -20,32 +20,32 @@ type (
 )
 
 // NewObserveRequest - создаёт объект StatRequest.
-func NewObserveRequest() *ObserveRequest {
+func NewObserveRequest(namespace string) *ObserveRequest {
 	return &ObserveRequest{
 		requestStatus: prometheus.NewHistogramVec(
 			prometheus.HistogramOpts{
-				Namespace: "http",
+				Namespace: namespace,
 				Subsystem: "request",
-				Name:      "status",
+				Name:      "duration_seconds",
 				Help:      "Request executed time",
-				Buckets:   []float64{0.005, 0.05, 0.5, 5},
+				Buckets:   []float64{0.001, 0.005, 0.025, 0.2, 1, 4, 8},
 			},
 			[]string{"status"},
 		),
 		requestSize: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Namespace: "http",
+				Namespace: namespace,
 				Subsystem: "request",
-				Name:      "size_total",
+				Name:      "size_bytes_total",
 				Help:      "Size in bytes of received information",
 			},
 			[]string{"method"},
 		),
 		responseSize: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Namespace: "http",
+				Namespace: namespace,
 				Subsystem: "response",
-				Name:      "size_total",
+				Name:      "size_bytes_total",
 				Help:      "Size in bytes of sent information",
 			},
 			[]string{"method"},
