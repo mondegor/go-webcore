de624f3abbbaf0bdeb7b27793d27ab313912ba57
Andrey Nazarov <mondegor@gmail.com>
2024-11-05 00:36:47 +0300

    See changelog 2024-11-05

--- mrserver/mrparser/image_options.go
+++ mrserver/mrparser/image_options.go
@@ -2,33 +2,33 @@ package mrparser
 
 type (
 	// ImageOption - настройка объекта Image.
-	ImageOption func(i *Image)
+	ImageOption func(im *Image)
 )
 
 // WithImageMaxWidth - устанавливает опцию maxWidth для Image (pixels).
 func WithImageMaxWidth(value uint64) ImageOption {
-	return func(i *Image) {
-		i.maxWidth = value
+	return func(im *Image) {
+		im.maxWidth = value
 	}
 }
 
 // WithImageMaxHeight - устанавливает опцию maxHeight для Image (pixels).
 func WithImageMaxHeight(value uint64) ImageOption {
-	return func(i *Image) {
-		i.maxHeight = value
+	return func(im *Image) {
+		im.maxHeight = value
 	}
 }
 
 // WithImageCheckBody - устанавливает опцию maxHeight для Image.
 func WithImageCheckBody(value bool) ImageOption {
-	return func(i *Image) {
-		i.checkBody = value
+	return func(im *Image) {
+		im.checkBody = value
 	}
 }
 
 // WithImageFileOptions - устанавливает опции File для Image.
 func WithImageFileOptions(opts ...FileOption) ImageOption {
-	return func(i *Image) {
-		i.file.applyOptions(opts)
+	return func(im *Image) {
+		im.file.applyOptions(opts)
 	}
 }
